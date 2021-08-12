2c8f0dacc44a2413544c1742870e9a7d10fa1703
unknown <bassbiteye45@gmail.com>
2021-08-12 22:55:16 +0000

    first commit

---
+++ Controllers/EtudiantsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TpGl2021Core.Models;
+
+namespace TpGl2021Core.Controllers
+{
+    public class EtudiantsController : Controller
+    {
+        private readonly CoreContext _context;
+
+        public EtudiantsController(CoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Etudiants
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.etudiants.ToListAsync());
+        }
+
+        // GET: Etudiants/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etudiant = await _context.etudiants
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (etudiant == null)
+            {
+                return NotFound();
+            }
+
+            return View(etudiant);
+        }
+
+        // GET: Etudiants/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Etudiants/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("prenom,Nom,Tel,Adresse")] Etudiant etudiant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(etudiant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etudiant);
+        }
+
+        // GET: Etudiants/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etudiant = await _context.etudiants.FindAsync(id);
+            if (etudiant == null)
+            {
+                return NotFound();
+            }
+            return View(etudiant);
+        }
+
+        // POST: Etudiants/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("prenom,Nom,Tel,Adresse")] Etudiant etudiant)
+        {
+            if (id != etudiant.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(etudiant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EtudiantExists(etudiant.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etudiant);
+        }
+
+        // GET: Etudiants/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etudiant = await _context.etudiants
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (etudiant == null)
+            {
+                return NotFound();
+            }
+
+            return View(etudiant);
+        }
+
+        // POST: Etudiants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var etudiant = await _context.etudiants.FindAsync(id);
+            _context.etudiants.Remove(etudiant);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EtudiantExists(int id)
+        {
+            return _context.etudiants.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Migrations/20210807143957_initialmigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TpGl2021Core.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "etudiants",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    prenom = table.Column<string>(maxLength: 160, nullable: false),
+                    Nom = table.Column<string>(maxLength: 160, nullable: false),
+                    Tel = table.Column<int>(nullable: false),
+                    Adresse = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_etudiants", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "etudiants");
+        }
+    }
+}
